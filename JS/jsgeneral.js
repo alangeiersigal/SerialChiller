f5cf935c3f434d8b8af5240c65db29c4636d1e51
Matteo <matteolovattoftw@gmail.com>
2019-11-08 13:11:29 -0300

    empeze js de generos y js general

---
+++ JS/jsgeneral.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function() {
+
+  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=46aea19a7447a9c4b1cd03a96834279e&language=en-US")
+  .then(function(response) {
+   return response.json()
+  })
+  .then(function (data) {
+   console.log(data);
+   var generos = data.genres;
+   var dropdown = document.querySelector(".dropdown-content")
+   for (var i = 0; i < generos.length; i++) {
+     dropdown.innerHTML += "<a href=generos.html?idgenres=" + data.genres[i].id + ">" + data.genres[i].name + "</a>";
+   }
+
+  })
+  .catch(function(error) {
+   console.log("Error: " + error);
+  })
+
+
+
+
+
+
+})
